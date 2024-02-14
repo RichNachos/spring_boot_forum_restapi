f95fbe6dc70b611b3ddcc4bc6c93974a9eedb602
RichNachos <gkldi20@freeuni.edu.ge>
2024-02-14 22:56:54 +0400

    Added new Comment model

--- src/main/java/com/richnachos/forum/entities/Comment.java
+++ src/main/java/com/richnachos/forum/entities/Comment.java
@@ -9,30 +9,26 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @Entity
-@Table(name = "posts")
+@Table(name = "comments")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Post {
+public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "poster_id", nullable = false)
-    private User poster;
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+    @ManyToOne
+    @JoinColumn(name = "commenter_id", nullable = false)
+    private User commenter;
 
-    @Column(name = "title", nullable = false)
-    private String title;
     @Column(name = "text", nullable = false)
     private String text;
     @Column(name = "upload_date", nullable = false)
     private Date uploadDate;
-
-    @PrePersist
-    protected void onCreate() {
-        uploadDate = new Date();
-    }
 }
